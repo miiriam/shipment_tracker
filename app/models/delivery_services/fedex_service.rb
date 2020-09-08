de15827f18fdbe3430e68f9323ed69c5537e5653
Miriam García <miiriamgar@gmail.com>
2020-09-08 18:51:34 -0500

    Adds Fedex implementation

---
+++ app/models/delivery_services/fedex_service.rb
@@ -0,0 +1,23 @@
+module DeliveryServices
+  class FedexService
+
+    def initialize
+      @fedex_client = fedex_client
+    end
+
+    def tracking_status(tracking_number)
+      tracking = fedex_client.track(tracking_number: tracking_number)
+      tracking.first.status
+    end
+  
+    def fedex_client
+      Fedex::Shipment.new(
+        key: ENV['FEDEX_KEY'],
+        password: ENV['FEDEX_PASSWORD'],
+        account_number: ENV['FEDEX_ACCOUNT_NUMBER'],
+        meter: ENV['FEDEX_METER'],
+        mode: ENV['FEDEX_MODE']
+      )
+    end
+  end
+end
