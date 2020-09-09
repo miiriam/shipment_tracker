7db5f5f44a74b989cbe188d80d16fa61622ae72d
Miriam García <miiriamgar@gmail.com>
2020-09-09 18:48:17 -0500

    Add Tracker controller

---
+++ app/controllers/api/v1/tracking_status_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::TrackingStatusController < ApplicationController
+  before_action :parse_request
+  # POST /api/v1/tracking_status/tracking_status
+  def tracking_status
+    # byebug
+    tracker = Tracker.new
+    response = tracker.status_info(@request)
+    render json: response
+  end
+
+  private
+
+  def parse_request
+    @request = JSON.parse(request.body.read)
+  end
+end
