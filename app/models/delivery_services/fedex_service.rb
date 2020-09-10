f760e428eab57ec23a94d29751e6a61ed56cd2a8
Miriam García <miiriamgar@gmail.com>
2020-09-10 17:09:50 -0500

    Creates a list for homologate Fedex status

--- app/models/delivery_services/fedex_service.rb
+++ app/models/delivery_services/fedex_service.rb
@@ -1,19 +1,38 @@
 module DeliveryServices
   class FedexService
 
+    STATUS_LIST = {
+      CREATED:
+       ['Shipment information sent to FedEx',
+        'At destination sort facility',
+        'At Pickup'],
+      ON_TRANSIT: [
+        'Departed FedEx location',
+        'On FedEx vehicle for delivery',
+        'At FedEx destination facility'
+      ],
+      DELIVERED: [
+        'Delivered'
+      ]
+    }
+
     def initialize
       @fedex_client = fedex_client
+      @status_list = STATUS_LIST
     end
 
     def tracking_status(tracking_number)
       begin 
         tracking = fedex_client.track(tracking_number: tracking_number)
-        tracking.first.status
+        fedex_status = tracking.first.status
+        homologate_status(fedex_status)
       rescue Fedex::RateError
         'EXCEPTION'
       end
     end
   
+    private 
+
     def fedex_client
       Fedex::Shipment.new(
         key: ENV['FEDEX_KEY'],
@@ -23,5 +42,12 @@ module DeliveryServices
         mode: ENV['FEDEX_MODE']
       )
     end
-  end
+
+    def homologate_status(fedex_status)
+      @status_list.each do |status|
+        return status[0].to_s if status[1].include?(fedex_status)
+      end
+    end
+  end  
 end
+
