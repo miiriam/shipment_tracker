1af87a27f3fea2078b570797a4b00435207510cc
Miriam García <miiriamgar@gmail.com>
2020-09-11 23:07:53 -0500

    DataBase configurations

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,5 @@ module ShipmentTracker
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
-    # Setting ActiveJob adapter
-    config.active_job.queue_adapter = :sidekiq
   end
 end
